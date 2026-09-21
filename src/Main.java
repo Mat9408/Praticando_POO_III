656a2d46f3e114277658218b8ed82fbd63ace134
Mat9408 <matheus94.ma@gmail.com>
2026-09-21 12:19:25 -0300

    Alteracoes na main

--- src/Main.java
+++ src/Main.java
@@ -8,22 +8,26 @@ import com.mt9408.ooppractice3.tabuada.TabuadaMultiplicacao;
 public class Main {
     public static void main(String[] args) {
 
-        /*Conversor de Moeda
+        //Conversor de Moeda
         ConversorMoeda conv1 = new ConversorMoeda();
-        conv1.converterDolarParaReal(100.00);*/
+        conv1.converterDolarParaReal(100.00);
 
-        /*CalculadoraSalaRetangular sala1 = new CalculadoraSalaRetangular();
+        //Calculadora de Área e Perímetro
+        CalculadoraSalaRetangular sala1 = new CalculadoraSalaRetangular();
         sala1.calcularArea(10,10);
 
-        sala1.calcularPerimetro(10,10,10,10);*/
+        sala1.calcularPerimetro(10,10,10,10);
 
-        /*TabuadaMultiplicacao num1 = new TabuadaMultiplicacao();
-        num1.mostrarTabuada(5);*/
+        //Tabuada
+        TabuadaMultiplicacao num1 = new TabuadaMultiplicacao();
+        num1.mostrarTabuada(5);
 
-        /*ConversorTemperaturaPadrao temp1 = new ConversorTemperaturaPadrao();
+        //Conversores de Temperatura
+        ConversorTemperaturaPadrao temp1 = new ConversorTemperaturaPadrao();
         temp1.celsiusParaFahrenheit(100);
-        temp1.fahrenheitParaCelsius(200);*/
+        temp1.fahrenheitParaCelsius(200);
 
+        //Cálculo de Preço
         Livro livro1 = new Livro();
         livro1.calculaPrecoFinal(100);
 
